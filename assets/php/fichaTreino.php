a422c0c67cd02129d23007c3d368f2432c4de15e
avelarheitor <heitorpereiraavelar@gmail.com>
2023-12-11 16:27:06 -0300

    heitor

--- assets/php/fichaTreino.php
+++ assets/php/fichaTreino.php
@@ -11,7 +11,7 @@ include_once("conexao.php");
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../css/fichaTreino.css">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-  <title>Editar Usuário</title>
+  <title>Ficha Aluno</title>
 </head>
 
 <body>
